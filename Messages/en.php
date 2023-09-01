4dafacc9f4a3f14f2152c85cd67b3d783fb402ed
Anonymous <noreply@weblate.org>
2023-09-01 11:45:12 +0000

    Translated using Weblate (English)
    Currently translated at 100.0% (46 of 46 strings)
    
    Translation: MIKOPBX/ModuleLdapSync
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/moduleldapsync/en/

--- Messages/en.php
+++ Messages/en.php
@@ -1,5 +1,16 @@
 <?php
-/*
+return [
+    'mo_ModuleModuleLdapSync' => 'Synchronization with LDAP/AD',
+    'BreadcrumbModuleLdapSync' => 'Synchronization with LDAP/AD',
+    'module_ldap_LdapServerConnectionHeader' => 'Domain controller connection settings',
+    'module_ldap_TableColumnServerName' => 'Server address',
+    'module_ldap_OrganizationalUnit' => 'Subdivision',
+    'module_ldap_UserNameAttribute' => 'user\'s first and last name',
+    'module_ldap_UserAccountControl' => 'attribute where the user\'s lock status is stored',
+    'module_ldap_LdapGetUsersButton' => 'Run request',
+    'module_ldap_usersSyncResult' => 'Status',
+    'module_ldap_UPDATED' => 'processed',
+    /*
  * MikoPBX - free phone system for small business
  * Copyright © 2017-2023 Alexey Portnov and Nikolay Beketov
  *
@@ -16,7 +27,40 @@
  * You should have received a copy of the GNU General Public License along with this program.
  * If not, see <https://www.gnu.org/licenses/>.
  */
-
-return [
-
+    'repModuleLdapSync' => 'Synchronization with LDAP/AD - %repesent%',
+    'SubHeaderModuleLdapSync' => 'The module allows you to load users from a domain',
+    'module_ldap_AddServer' => 'Add server',
+    'module_ldap_AddServerShort' => 'Add',
+    'module_ldap_NoAnyServers' => 'No servers configured',
+    'module_ldap_BaseDN' => 'Domain root',
+    'module_ldap_ldapType' => 'Server type',
+    'module_ldap_LdapServerName' => 'Domain controller address',
+    'module_ldap_LdapServerPort' => 'Port',
+    'module_ldap_LdapAdminLogin' => 'Username and password with read/write access to the domain',
+    'module_ldap_LdapBaseDN' => 'Domain root',
+    'module_ldap_LdapPassword' => 'Password',
+    'module_ldap_LdapAttributesHeader' => 'Attributes in the domain for matching with data in MikoPBX',
+    'module_ldap_UserExtensionAttribute' => 'user extension number',
+    'module_ldap_UserMobileAttribute' => 'mobile phone',
+    'module_ldap_UserEmailAttribute' => 'E-mail address',
+    'module_ldap_UserAvatarAttribute' => 'photo attribute',
+    'module_ldap_UpdateAttributes' => 'Update phone numbers in the domain when they change in MikoPBX (requires write permissions)',
+    'module_ldap_LdapOrganizationalUnit' => 'Subdivision',
+    'module_ldap_LdapUserFilter' => 'Additional user filter',
+    'module_ldap_LdapCheckGetListHeader' => 'Test to get list of LDAP users',
+    'module_ldap_LdapCheckGetUsersList' => 'Using the specified access parameters and filters, we will query LDAP / AD and get a list of users to synchronize',
+    'module_ldap_user_not_found' => 'The user does not have access to the domain, or the parameters specified are incorrect',
+    'module_ldap_ValidateServerNameIsEmpty' => 'Domain controller address not filled',
+    'module_ldap_ValidateServerPortIsEmpty' => 'Domain controller port not populated',
+    'module_ldap_ValidateAdministrativeLoginIsEmpty' => 'Login for domain user is not filled',
+    'module_ldap_ValidateAdministrativePasswordIsEmpty' => 'Password not filled for domain user',
+    'module_ldap_ValidateBaseDNIsEmpty' => 'Domain root not filled',
+    'module_ldap_ValidateUserIdAttributeIsEmpty' => 'The attribute with the username in the domain is not filled',
+    'module_ldap_LdapManualSyncHeader' => 'Manual sync',
+    'module_ldap_LdapManualSyncManual' => 'Clicking the button will synchronize users between the specified domain controller and MikoPBX',
+    'module_ldap_LdapManualSyncButton' => 'Synchronize data',
+    'module_ldap_userHadChangesOnTheSide' => 'Updated',
+    'module_ldap_OnPBXSide' => 'inside MikoPBX',
+    'module_ldap_OnDomainSide' => 'in the domain',
+    'module_ldap_SKIPPED' => 'skipped',
 ];
