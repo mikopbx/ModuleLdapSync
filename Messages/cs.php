cfab80bf3947e124a71ae8f829104bb4053f09f4
Anonymous <noreply@weblate.org>
2024-09-17 07:27:03 +0000

    Translated using Weblate (Czech)
    Currently translated at 100.0% (65 of 65 strings)
    
    Translation: MIKOPBX/ModuleLdapSync
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/moduleldapsync/cs/

--- Messages/cs.php
+++ Messages/cs.php
@@ -78,4 +78,8 @@ return [
     'module_ldap_ConflictErrorMessages' => 'Důvod odmítnutí',
     'module_ldap_PBX_UPDATE_CONFLICT' => 'PBX',
     'module_ldap_LDAP_UPDATE_CONFLICT' => 'LDAP/AD',
+    'module_ldap_CONFLICT' => 'konflikt',
+    'module_ldap_SyncTabHeader' => 'Synchronizace a konflikty',
+    'module_ldap_TabAttributes' => 'Synchronizační pole',
+    'module_ldap_DeleteAllConflicts' => 'Vymažte všechny konflikty',
 ];
