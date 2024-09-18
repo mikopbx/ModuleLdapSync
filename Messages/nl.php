2ee9008397e99f8f5a8c86a394a5ba75c54d76d1
Anonymous <noreply@weblate.org>
2024-09-18 04:46:53 +0000

    Translated using Weblate (Dutch)
    Currently translated at 98.6% (72 of 73 strings)
    
    Translation: MIKOPBX/ModuleLdapSync
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/moduleldapsync/nl/

--- Messages/nl.php
+++ Messages/nl.php
@@ -85,4 +85,9 @@ return [
     'module_ldap_UserPasswordAttribute' => 'SIP-wachtwoord',
     'module_ldap_UseTLS' => 'TLS/SSL',
     'module_ldap_UpdateAttributesMessage' => 'Wanneer gegevens in MikoPBX veranderen, wordt in het domein het volgende bijgewerkt: intern nummer, mobiel nummer, e-mail, avatar, SIP-wachtwoord',
+    'module_ldap_UserName' => 'Gebruikersnaam',
+    'module_ldap_UserNumber' => 'Extensienummer',
+    'module_ldap_findExtension' => 'Zoek in de lijst met gebruikers',
+    'module_ldap_DeletedUsersHeader' => 'Medewerkers op afstand in LDAP/AD',
+    'module_ldap_DeletedUsersEmpty' => 'Geen externe medewerkers',
 ];
