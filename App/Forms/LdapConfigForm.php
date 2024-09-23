7f7406f5bd0f73dcf67163580c655492541a11cc
Nikolay Beketov <nbek@miko.ru>
2024-09-23 11:04:51 +0800

    Cleanup

--- App/Forms/LdapConfigForm.php
+++ App/Forms/LdapConfigForm.php
@@ -84,7 +84,7 @@ class LdapConfigForm extends BaseForm
             'DirectoryServer' => 'DirectoryServer',
             'FreeIPA' => 'FreeIPA',
         ];
-        $extension = new Select(
+        $ldapType = new Select(
             'ldapType', $types, [
                 'using' => [
                     'id',
@@ -95,7 +95,7 @@ class LdapConfigForm extends BaseForm
                 'class' => "ui selection dropdown select-ldap-field",
             ]
         );
-        $this->add($extension);
+        $this->add($ldapType);
 
 
         $attributes = json_decode($entity->attributes??'', true);
